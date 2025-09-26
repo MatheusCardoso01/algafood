aeb983619ec2a2ba6215d2bbaa9bc0dc52fb9a53
Matheus Ferreira Cardoso <matheus.cardoso@squadra.com.br>
2025-09-26 16:50:14 -0300

    Notações relacionadas a persistência de dados: Transactions, PersistenceContext, Id, GeneratedValue e classes como ApplicationContext

--- src/main/java/com/algaworks/algafoodapi/jpa/AlteracaoCozinhaMain.java
+++ src/main/java/com/algaworks/algafoodapi/jpa/AlteracaoCozinhaMain.java
@@ -1,4 +1,46 @@
 package com.algaworks.algafoodapi.jpa;
 
+import com.algaworks.algafoodapi.AlgafoodApiApplication;
+import com.algaworks.algafoodapi.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
 public class AlteracaoCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        Cozinha cozinha1 = new Cozinha();
+        Cozinha cozinha3 = new Cozinha();
+        cozinha1.setId(1L);
+        cozinha1.setNome("Brasileira");
+        cozinha3.setId(3L);
+        cozinha3.setNome("Lusitana");
+
+        cadastroCozinha.salvar(cozinha1);
+        cadastroCozinha.salvar(cozinha3);
+
+        List<Cozinha> cozinhas = cadastroCozinha.listar();
+
+        for (Cozinha cozinha : cozinhas) {
+            System.out.println(cozinha.getNome());
+        }
+
+        cadastroCozinha.remover(cozinha3);
+        System.out.println("==========================");
+        cozinhas = cadastroCozinha.listar();
+
+        for (Cozinha cozinha : cozinhas) {
+            System.out.println(cozinha.getNome());
+        }
+
+    }
+
 }
